8017089c1bad9d11236f2b10def5f58f0f03329d
hammamfaiz <hammamfaiz36@gmail.com>
2022-11-19 21:59:53 +0700

    feat: add icon

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,11 +1,15 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header class="text-gray-600 body-font">
       <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <svg
+        <Link
+          href="/"
+          class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+        >
+          {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             stroke="currentColor"
@@ -16,9 +20,10 @@ export default function Header() {
             viewBox="0 0 24 24"
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
+          </svg> */}
+          <Image src="/assets/icon.png" width="40" height="40" />
           <span class="ml-3 text-xl">Zoo</span>
-        </a>
+        </Link>
         <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
           <Link class="mr-5 hover:text-gray-900" href="/">
             Our List
