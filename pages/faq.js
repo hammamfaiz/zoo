18b7d8047fb0c2df49840b20a6b668244575b8c7
hammamfaiz <hammamfaiz36@gmail.com>
2022-12-01 14:08:57 +0700

    feat: final

--- pages/faq.js
+++ pages/faq.js
@@ -25,18 +25,22 @@ export default function FAQ() {
                 >
                   <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
                 </svg>
-                <p className="leading-relaxed mb-6">Pertanyaan</p>
+                <p className="leading-relaxed mb-6">
+                  Berikut adalah GitHub saya
+                </p>
                 <a
                   className="inline-flex items-center"
-                  href="https://rapidapi.com/ahmadawais/api/zoo-animals-api/"
+                  href="https://github.com/hammamfaiz"
                   rel="nooperner noreferrer"
                   target="_blank"
                 >
                   <span className="flex-grow flex flex-col">
                     <span className="title-font font-medium text-gray-900">
-                      gatau
+                      Hammam Faiz
                     </span>
-                    <span className="text-gray-500 text-sm">123</span>
+                    <span className="text-gray-500 text-sm">
+                      Teknik Komputer 2020
+                    </span>
                   </span>
                 </a>
               </div>
@@ -51,18 +55,25 @@ export default function FAQ() {
                 >
                   <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
                 </svg>
-                <p className="leading-relaxed mb-6">Pertanyaan</p>
+                <p className="leading-relaxed mb-6">
+                  Saya menggunakan Vercel sebagai deployment. Perusahaan
+                  memelihara kerangka kerja pengembangan web Next.js. Arsitektur
+                  Vercel dibangun di sekitar Jamstack, dan penerapan ditangani
+                  melalui repositori Git.
+                </p>
                 <a
                   className="inline-flex items-center"
-                  href="https://rapidapi.com/ahmadawais/api/zoo-animals-api/"
+                  href="https://vercel.com/hammamfaiz/zoo"
                   rel="nooperner noreferrer"
                   target="_blank"
                 >
                   <span className="flex-grow flex flex-col">
                     <span className="title-font font-medium text-gray-900">
-                      gatau
+                      Ezra Lazuardy Wijaya
                     </span>
-                    <span className="text-gray-500 text-sm">123</span>
+                    <span className="text-gray-500 text-sm">
+                      Teknik Komputer 2020
+                    </span>
                   </span>
                 </a>
               </div>
@@ -79,19 +90,21 @@ export default function FAQ() {
                 </svg>
                 <p className="leading-relaxed mb-6">
                   Harus menggunakan API (Web Service). Disini saya menggunakan
-                  API dari rapidapi.com
+                  JSON
                 </p>
                 <a
                   className="inline-flex items-center"
-                  href="https://rapidapi.com/ahmadawais/api/zoo-animals-api/"
+                  href="https://jsonbin.io/app/bins"
                   rel="nooperner noreferrer"
                   target="_blank"
                 >
                   <span className="flex-grow flex flex-col">
                     <span className="title-font font-medium text-gray-900">
-                      Zoo Animals API
+                      Ida Bagus Putu Putra Manuaba
                     </span>
-                    <span className="text-gray-500 text-sm"></span>
+                    <span className="text-gray-500 text-sm">
+                      Teknik Komputer 2020
+                    </span>
                   </span>
                 </a>
               </div>
@@ -107,15 +120,24 @@ export default function FAQ() {
                   <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
                 </svg>
                 <p className="leading-relaxed mb-6">
-                  Memiliki 5 activity. 2 halaman utama list dan detail dan 1
-                  halaman about aplikasi
+                  Untuk tampilannya, saya menggunakan Tailwindcss. kerangka
+                  kerja (framework) CSS yang di dalamnya terdapat sekumpulan
+                  utility classes untuk membangun antarmuka kustom dengan cepat.
                 </p>
+                <a
+                  className="inline-flex items-center"
+                  href="https://tailwindcss.com/docs/guides/nextjs"
+                  rel="nooperner noreferrer"
+                  target="_blank"
+                ></a>
                 <a className="inline-flex items-center">
                   <span className="flex-grow flex flex-col">
                     <span className="title-font font-medium text-gray-900">
-                      Alper Kamu
+                      Muhammad Salman aka Moon
                     </span>
-                    <span className="text-gray-500 text-sm">DESIGNER</span>
+                    <span className="text-gray-500 text-sm">
+                      Teknik Komputer 2020
+                    </span>
                   </span>
                 </a>
               </div>
@@ -130,18 +152,22 @@ export default function FAQ() {
                 >
                   <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
                 </svg>
-                <p className="leading-relaxed mb-6">Pertanyaan</p>
+                <p className="leading-relaxed mb-6">
+                  Untuk template saya menggunakan Tailblocks
+                </p>
                 <a
                   className="inline-flex items-center"
-                  href="https://rapidapi.com/ahmadawais/api/zoo-animals-api/"
+                  href="https://tailblocks.cc/"
                   rel="nooperner noreferrer"
                   target="_blank"
                 >
                   <span className="flex-grow flex flex-col">
                     <span className="title-font font-medium text-gray-900">
-                      gatau
+                      Didan Hasan
                     </span>
-                    <span className="text-gray-500 text-sm">123</span>
+                    <span className="text-gray-500 text-sm">
+                      Teknik Komputer 2020
+                    </span>
                   </span>
                 </a>
               </div>
@@ -156,20 +182,55 @@ export default function FAQ() {
                 >
                   <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
                 </svg>
-                <p className="leading-relaxed mb-6">Pertanyaan</p>
+                <p className="leading-relaxed mb-6">
+                  Untuk framework yang digunakan itu Next.js. kerangka kerja
+                  pengembangan web sumber terbuka yang dibuat oleh Vercel yang
+                  memungkinkan aplikasi web berbasis React dengan rendering sisi
+                  server dan menghasilkan situs web statis.
+                </p>
                 <a
                   className="inline-flex items-center"
-                  href="https://rapidapi.com/ahmadawais/api/zoo-animals-api/"
+                  href="https://nextjs.org/"
                   rel="nooperner noreferrer"
                   target="_blank"
                 >
                   <span className="flex-grow flex flex-col">
                     <span className="title-font font-medium text-gray-900">
-                      gatau
+                      Kakung Bangkit
                     </span>
-                    <span className="text-gray-500 text-sm">123</span>
+                    <span className="text-gray-500 text-sm">
+                      Teknik Komputer 2020
+                    </span>
                   </span>
                 </a>
+              </div>
+            </div>
+            <div className="p-4 md:w-1/2 w-full">
+              <div className="h-full bg-gray-100 p-8 rounded">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  className="block w-5 h-5 text-gray-400 mb-4"
+                  viewBox="0 0 975.036 975.036"
+                >
+                  <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                </svg>
+                <p className="leading-relaxed mb-6">
+                  Service Worker adalah merupakan salah satu API (Application
+                  Programming Interface) Javascript yang memungkinkan pengembang
+                  dalam melakukan pemrograman cache dan melakukan load asset
+                  data, melakukan manajemen push notification, dan yang lainnya.
+                  Service Workernya pakai library dari next-pwa yang aka
+                  megenerate otomatis
+                </p>
+                <span className="flex-grow flex flex-col">
+                  <span className="title-font font-medium text-gray-900">
+                    Hammam Faiz
+                  </span>
+                  <span className="text-gray-500 text-sm">
+                    Teknik Komputer 2020
+                  </span>
+                </span>
               </div>
             </div>
           </div>
